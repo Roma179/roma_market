f31b47c00a5e9971032d848138b8811dabac0f30
roma179 <r_cpj17@comunidad.unam.mx>
2023-01-28 12:32:19 -0600

    mi segundo commit

---
+++ src/main/java/com/roma/market/RomaMarketApplication.java
@@ -0,0 +1,13 @@
+package com.roma.market;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RomaMarketApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RomaMarketApplication.class, args);
+	}
+
+}
